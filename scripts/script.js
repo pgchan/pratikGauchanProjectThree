e9bc9167f88ce8dfbb67b4d93196665b859ac483
Pratik <pgauchan@gmail.com>
2018-08-13 23:04:29 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+/*
+
+empty cart variable array that holds all the selected items.
+    - price adds all the indexes inside the array
+    - cart dynamically shows the number of items in basket (array.length)
+
+
+
+
+
+1. section - select pizza
+
+    on click, select one pizza. if pizza:selected is not equal to one, show error sign "please select a pizza to continue" on checkout
+
+
+
+2. section - select cheese
+
+    on click, select any number of cheeses. 
+
+
+
+
+3. section - select veggies
+
+    on click, select any number of veggies.
+
+
+
+
+4. section - select meat
+
+    on click, select any number of meat. 
+
+
+
+
+5. section - submit
+
+
+
+
+
+
+1. Global price = 0. If anything is selected, add to total price. If anything is taken out, subtract
+2. Select size - Small($7.50)/ Medium($9.00) /Large ($11.00)
+3. Select toppings -
+    1. Veggies ($1.50) - artichokes, black olives, mushrooms, Roma tomatoes, pineapples
+    2. Meat ($2.50)- bacon, pepperoni,  salami,
+    3. Cheese ($0.50)- Feta, Goat cheese, Parmesan
+4. Add adsSelect toppings -
+    1. Veggies ($1.50) - artichokes, black olives, mushrooms, Roma tomatoes, pineapples
+    2. Meat ($2.50)- bacon, pepperoni,  salami,
+    3. Cheese ($0.50)- Feta, Goat cheese, Parmesan
+
+
+
+
+
+
+
+
+
+
+
+*/
