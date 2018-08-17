04444034dbf1298481e64ead4059cd0bc82d86cc
Pratik <pgauchan@gmail.com>
2018-08-17 00:41:14 -0400

    started javascript

--- scripts/script.js
+++ scripts/script.js
@@ -19,113 +19,93 @@ empty cart variable array that holds all the selected items.
 5. section - submit
 
 
-
-
-
-
-1. Global price = 0. If anything is selected, add to total price. If anything is taken out, subtract
-2. Select size - Small($7.50)/ Medium($9.00) /Large ($11.00)
-3. Select toppings -
-    1. Veggies ($1.50) - artichokes, black olives, mushrooms, Roma tomatoes, pineapples
-    2. Meat ($2.50)- bacon, pepperoni,  salami,
-    3. Cheese ($0.50)- Feta, Goat cheese, Parmesan
-4. Add adsSelect toppings -
-    1. Veggies ($1.50) - artichokes, black olives, mushrooms, Roma tomatoes, pineapples
-    2. Meat ($2.50)- bacon, pepperoni,  salami,
-    3. Cheese ($0.50)- Feta, Goat cheese, Parmesan
-
-
 */
 
 
-// <section class="wrapper">
-//     <form action="">
-//         <fieldset class="pizza-toppings-cheese">
-//             <label for="feta">feta</label>
-//             <input type="radio" name="feta" id="feta" value="feta">
-//                 <label for="goat cheese">goat cheese</label>
-//                 <input type="radio" name="goat cheese" id="goat cheese" value="goat cheese">
-//                     <label for="mozzarella">mozzarella</label>
-//                     <input type="radio" name="mozzarella" id="mozzarella" value="mozzarella">
-//         </fieldset>
-//     </form>
-// </section>
 
 
 
+const menu = {}
 
-
-
-const pizza = {}
-
-pizza.toppingsCheese = {
+menu.size = {
     small: 8.50,
     medium: 10,
     large: 11.50,
 }
 
-pizza.contents = [];
+// menu.cheeseTopping = {
+//     feta: 2,
+//     goat: 2,
+//     mozzarella: 2,
+// }
+
+// pizza.veggies = {
+
+// }
+
+let order = {};
+
+order = {
+    pizzaSize: "",
+    selectedCheese: [],
+    vegetableToppings: [],
+    meatTopping: [],
+}
+
+const pizza_cost = 0;
 
 
-$(function() {
+$(document).ready(function () {
 
-    $('.pizza-size-clickable').change(function(event) {
-        event.preventDefault();
+        $('.pizza-size-clickable').change(function(event) {
+            event.preventDefault();
 
-        const pizzaSize = $('input[name=pizza-size]:checked').val();
-        console.log(pizzaSize);
+            order.pizzaSize = $('input[name=pizza-size]:checked').val();
+            
+            // order.pizzaPrice = menu.size[pizzaSize];
+            // order.
 
-        // pizza.contents.push(pizzaSize);
-        pizza.contents[0] = pizzaSize
-
-        console.log(pizza.contents)
-
+        })
 
 
+    // make a new array called selectedCheese. When a cheese is selected, push the value to selectedCheese array.
+   //  loop through the selectedCheese array.
+
+        order.cheese = [];
+        
+        $(".pizza-cheese input[type='checkbox']").click(function () {
+
+            const cheeseValue = $(this).val();
+
+            if (order.cheese.includes(cheeseValue)) {
+                order.cheese = order.cheese.filter(cheese => cheese != cheeseValue);      
+                
+            } else {
+                order.cheese.push(cheeseValue);
+            }
+
+            console.log(order.cheese);
+
+        })
+
+
+        // $('.pizza-topping-cheese-inner').on('click', function (event){
+    
+
+        //     order.cheeseTopping = $('input[name=cheese]:checked').val();
+        //     // const cheesePrice = pizza.cheeseTopping[cheeseValue]
+
+        //     // pizza.contents.cheeseValue = cheeseValue;
+        //     // pizza.contents.cheesePrice = cheesePrice;
+
+        //     // console.log(pizza.contents);
+
+
+        // })
+
+});
 
 
 
 
-
-
-
-
-
-
-    })
-
-
-
-
-})
-
-
-
-
-
-
-
-
-
-    // <section class="wrapper" >
-    //     <form action="">
-    //         <fieldset class="pizza-size">
-
-    //             <div class="pizza-size-contents">
-    //                 <label for="small">small</label>
-    //                 <input type="radio" name="pizza-size" id="small" value="small">
-    //                 <p>$8.50</p>
-    //             </div>
-    //             <div class="pizza-size-contents">
-    //                 <label for="medium">medium</label>
-    //                 <input type="radio" name="pizza-size" id="medium" value="medium">
-    //                 <p>$10.00</p>
-    //             </div>
-    //             <div class="pizza-size-contents">
-    //                 <label for="large">large</label>
-    //                 <input type="radio" name="pizza-size" id="large" value="large">
-    //                 <p>$11.50</p>
-    //             </div>
-    //         </fieldset>
-    //     </form>
-    // </section>
+// when pizzaSize is selected, inside object, pizza.size[pizzaSize] gets pushed to array called piza
