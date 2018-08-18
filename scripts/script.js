f0b21b7ca90bd2eb141b4599e65b2ac9554d6782
Pratik <pgauchan@gmail.com>
2018-08-18 11:40:29 -0400

    added more js, toppings

--- scripts/script.js
+++ scripts/script.js
@@ -7,25 +7,25 @@ menu.size = {
 }
 
 menu.cheese = {
-    feta: 2,
-    goat: 2,
-    mozzarella: 2,
+    feta: 1.5,
+    goat: 1.5,
+    mozzarella: 1.5,
 }
 
 menu.vegetables = {
-    artichokes: 1.5,
-    blackOlives: 1.5,
-    broccolli: 1.5,
-    mushrooms: 1.5,
-    romaTomato: 1.5,
-    pineapples: 1.5,
+    artichokes: 2.50,
+    blackOlives: 2.50,
+    broccolli: 2.50,
+    mushrooms: 2.50,
+    romatomatoes: 2.50,
+    pineapples: 2.50,
 }
 
-
-
-// pizza.veggies = {
-
-// }
+menu.meat = {
+    bacon: 4,
+    pepperoni: 4,
+    buffaloChicken: 4,
+}
 
 let order = {};
 
@@ -36,87 +36,108 @@ order = {
     meat: [],
 }
 
-const pizza_cost = 0;
+let pizzaCost = 0;
 
 $(document).ready(function () {
+/*
+    1. purpose of the function is to get the total price
+    2. see which items have been selected.
+    3. add the total price of the selected items together.
+    4. print it out.
+*/
+    function addPrice() {
+        pizzaCost = 0;
+        let sizePrice = 0;
+        let cheesePrice = 0;
+        let vegetablesPrice = 0;
+        let meatPrice = 0;
 
-        $('.pizza-size-clickable').change(function(event) {
-            event.preventDefault();
-            order.size = $('input[name=pizza-size]:checked').val();
-            // order.pizzaPrice = menu.size[pizzaSize];
-            // order.
-        });
-
-    // make a new array called selectedCheese. When a cheese is selected, push the value to selectedCheese array.
-   //  loop through the selectedCheese array.
-
-        order.cheese = [];    
-        $(".pizza-cheese input[type='checkbox']").click(function () {
-            const cheeseValue = $(this).val();
-            if (order.cheese.includes(cheeseValue)) {
-                order.cheese = order.cheese.filter(cheese => cheese != cheeseValue);          
-            } else {
-                order.cheese.push(cheeseValue);
-            }
-            console.log(order.cheese);
-        });
-
-        // order.vegetables = [];
-        // $(".pizza-vegetables input[type='checkbox']").click(function () {
-        //     const vegetablesValue = $(this).val();
-        //     if (order.vegetables.includes(vegetablesValue)) {
-        //         order.vegetables = order.vegetables.filter((vegetable) => {
-        //             return vegetable != vegetablesValue;
-        //         });
-        //     } else {
-        //         order.vegetables.push(vegetablesValue);
+        // if pizza size is not selected, then the size price should be 0. if pizza size is selected, size price should be the selecetd pizza's size price.
+         
+        // if (typeof order.size === 'undefined' || order.size.length === 0) {
+        //     sizePrice = 0;
+        // } else {
+        //     sizePrice = menu.size[order.size];
+        // }
+        
+        // if (order.cheese.length === 0) {
+        //     cheesePrice = 0;
+        // } else {
+        //     // sets price of cheese
+        //     for (let i = 0; i < order.cheese.length; i++) {
+        //         cheesePrice = cheesePrice + menu.cheese[order.cheese[i]];
         //     }
-        //     console.log(order.vegetables);
-        // });
 
-        order.vegetables = [];
-        $(".pizza-vegetables input[type='checkbox']").click(function(){
-            const vegetablesValue = $(this).val();
-            if (order.vegetables.includes(vegetablesValue)) {
-                order.vegetables = order.vegetables.filter((vegetable) => { 
-                    if (vegetable != vegetablesValue) {
-                        return;
-                    }     
-                });
-            } else {
-                order.vegetables.push(vegetablesValue);
-            }
-            console.log(order.vegetables);
-        });
+        // }
 
-        order.meat = [];
-        $(".pizza-meat input[type='checkbox']").click(function(){
-            const meatValue = $(this).val();
-            if(order.meat.includes(meatValue)) {
-                order.meat = order.meat.filter(meat => meat != meatValue);
-            } else {
-                order.meat.push(meatValue);
-            }
-            console.log(order.meat);
-        });
 
-        // $('.pizza-topping-cheese-inner').on('click', function (event){
+
+        // sets price of vegetables
+        for (let i = 0; i < order.vegetables.length; i++) {
+            vegetablesPrice = vegetablesPrice + menu.vegetables[order.vegetables[i]]
+        }
+
+        //sets price of meat
+        for (let i = 0; i < order.meat.length; i++) {
+            meatPrice = meatPrice + menu.meat[order.meat[i]];
+        }
+
+        // console.log(`size price - ${sizePrice}. cheese price ${cheesePrice}. veggie price ${vegetablesPrice}`)
+
+        //adds total
+        pizzaCost = sizePrice + cheesePrice + vegetablesPrice
+        //  + meatPrice;
+  
+        console.log(`Total price is - $ ${pizzaCost}`);
+    }
+
+
+
+
+    $('.pizza-size-clickable').change(function(event) {
+        event.preventDefault();
+        order.size = $('input[name=pizza-size]:checked').val();
+        addPrice();
+    });
+
+    order.cheese = []; 
+    $(".pizza-cheese input[type='checkbox']").click(function () {
+        const cheeseValue = $(this).val();
+        if (order.cheese.includes(cheeseValue)) {
+            order.cheese = order.cheese.filter(cheese => cheese != cheeseValue);       
+        } else {
+            order.cheese.push(cheeseValue);
+        }
+        addPrice();
+        // console.log(order.cheese);
+    });
+
     
+    order.vegetables = [];
+    $(".pizza-vegetables input[type='checkbox']").click(function () {
+        const vegetablesValue = $(this).val();
+        if (order.vegetables.includes(vegetablesValue)) {
+            order.vegetables = order.vegetables.filter(vegetable => vegetable != vegetablesValue);
+        } else {
+            order.vegetables.push(vegetablesValue);
+        }
+        addPrice();
+        // console.log(order.vegetables);
+    });
 
-        //     order.cheeseTopping = $('input[name=cheese]:checked').val();
-        //     // const cheesePrice = pizza.cheeseTopping[cheeseValue]
+    order.meat = [];
+    $(".pizza-meat input[type='checkbox']").click(function(){
+        const meatValue = $(this).val();
+        if(order.meat.includes(meatValue)) {
+            order.meat = order.meat.filter(meat => meat != meatValue);
+        } else {
+            order.meat.push(meatValue);
+        }
+        addPrice();
+        // console.log(order.meat);
+    });
 
-        //     // pizza.contents.cheeseValue = cheeseValue;
-        //     // pizza.contents.cheesePrice = cheesePrice;
-
-        //     // console.log(pizza.contents);
-
-
-        // })
 
 });
 
 
-
-
-// when pizzaSize is selected, inside object, pizza.size[pizzaSize] gets pushed to array called piza
